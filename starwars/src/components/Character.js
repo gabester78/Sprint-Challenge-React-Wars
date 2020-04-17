2d4d85bbf513e8a3fea59992e14a63d797ca769f
Gabriel Romero <gabester78@gmail.com>
2020-04-17 13:02:10 -0500

    All set.

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -4,32 +4,64 @@ import CharacterCard from "./CharacterCard";
 
 export default function Character() {
   //   const [char, setChar] = useState([]);
+  //   //   const [char2, setChar2] = useState([]);
   //   useEffect(() => {
   //     axios
-  //       .get("https://swapi.co/api/people/1")
+  //       //   .get("https://rickandmortyapi.com/api/character")
+  //       .get("https://swapi.py4e.com/api/people/")
   //       .then((response) => {
-  //         console.log(response.data.results);
+  //         console.log(response.data);
+  //         // console.log(response.data.results[1]);
   //         // const getResults = Object.values(response.data.results);
   //         // getResults.map((item) => {
-  //         setChar(response.data.results);
+  //         // response.data.results.forEach((data) => {
+  //         //   setChar(response.data);
+  //         // });
+  //         // setChar(response.data.results[0]);
+  //         // setChar2(response.data.results[1]);
+  //         setChar(response.data);
   //       })
   //       .catch((error) => {
   //         console.log("the data was not returned", error);
   //       });
   //   }, []);
 
+  //   return (
+  //     <div>
+  //       {char.map((e) => {
+  //         return (
+  //           <CharacterCard name={e.name} hair={e.hair_color} dob={e.birth_year} />
+  //         );
+  //       })}
+  //     </div>
+  //   );
+  // }
+
+  const [char, setChar] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://swapi.py4e.com/api/people/")
+      .then((response) => {
+        console.log(response.data);
+        setChar(response.data.results);
+      })
+      .catch((error) => {
+        console.log("the data was not returned", error);
+      });
+  }, []);
+
   return (
     <div>
-      {/* {char.map((data) => { */}
-      {/* return ( */}
-      <CharacterCard
-      // name={data.name}
-      // hair={data.hair_color}
-      // dob={data.birth_year}
-      // home={data.homeworld}
-      />
-      {/* );
-      })} */}
+      {char.map((data) => {
+        return (
+          <CharacterCard
+            name={data.name}
+            hair={data.hair_color}
+            dob={data.birth_year}
+            home={data.homeworld}
+          />
+        );
+      })}
     </div>
   );
 }
--- starwars/src/components/CharacterCard.js
+++ starwars/src/components/CharacterCard.js
@@ -6,28 +6,28 @@ import {
   DropdownItem,
 } from "reactstrap";
 
-const CharacterCard = (props) => {
+const CharacterCard = ({ name, hair, dob, home }) => {
   const [dropdownOpen, setDropdownOpen] = useState(false);
 
   const toggle = () => setDropdownOpen((prevState) => !prevState);
 
   return (
     <Dropdown isOpen={dropdownOpen} toggle={toggle}>
-      <DropdownToggle className="bg-info w-25 p-3" caret>
-        <h1>Luke</h1>
+      <DropdownToggle className="bg-info w-50 p-3 mb-5" caret>
+        <h1>{name}</h1>
       </DropdownToggle>
-      <DropdownMenu className="bg-dark text-light w-25 p-3">
+      <DropdownMenu className="bg-dark text-light w-50 p-3">
         <DropdownItem className="text-light">
-          <h4>Hair color: Hair</h4>
+          <h4>Hair color: {hair}</h4>
         </DropdownItem>
         <DropdownItem divider />
         <DropdownItem className="text-light">
-          <h4>Date of birth: Hair</h4>
+          <h4>Date of birth: {dob}</h4>
         </DropdownItem>
-        <DropdownItem divider />
+        {/* <DropdownItem divider />
         <DropdownItem className="text-light">
-          <h4>Homeworld: hair</h4>
-        </DropdownItem>
+          <h4>Homeworld: {home} </h4>
+        </DropdownItem> */}
       </DropdownMenu>
     </Dropdown>
   );
