e6e9baa7716e63f9dc3d5e06d560078e17a7bf08
Gabriel Romero <gabester78@gmail.com>
2020-04-17 11:41:29 -0500

    Dropdown styled.

--- starwars/src/components/CharacterCard.js
+++ starwars/src/components/CharacterCard.js
@@ -13,15 +13,21 @@ const CharacterCard = (props) => {
 
   return (
     <Dropdown isOpen={dropdownOpen} toggle={toggle}>
-      <DropdownToggle caret>
+      <DropdownToggle className="bg-info w-25 p-3" caret>
         <h1>Luke</h1>
       </DropdownToggle>
-      <DropdownMenu>
-        <h3>Hair</h3>
+      <DropdownMenu className="bg-dark text-light w-25 p-3">
+        <DropdownItem className="text-light">
+          <h4>Hair color: Hair</h4>
+        </DropdownItem>
         <DropdownItem divider />
-        <DropdownItem>DOB</DropdownItem>
+        <DropdownItem className="text-light">
+          <h4>Date of birth: Hair</h4>
+        </DropdownItem>
         <DropdownItem divider />
-        <DropdownItem>Home World</DropdownItem>
+        <DropdownItem className="text-light">
+          <h4>Homeworld: hair</h4>
+        </DropdownItem>
       </DropdownMenu>
     </Dropdown>
   );
