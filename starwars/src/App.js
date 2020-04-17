9e0fcb196d9db98c07084e11c52700845a57dc7d
Gabriel Romero <gabester78@gmail.com>
2020-04-17 10:37:48 -0500

    api up and running.

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./App.css";
-import CharacterCard from "./components/CharacterCard";
+import axios from "axios";
+import Character from "./components/Character";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -9,8 +11,33 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const [char, setChar] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://swapi.co/api/people")
+      .then((response) => {
+        console.log(response.data);
+        setChar(response.data.results);
+      })
+      .catch((error) => {
+        console.log("the data was not returned", error);
+      });
+  }, []);
 
-  return <CharacterCard />;
+  return (
+    <div>
+      {char.map((data) => {
+        return (
+          <Character
+            name={data.name}
+            hair={data.hair_color}
+            dob={data.birth_year}
+            home={data.homeworld}
+          />
+        );
+      })}
+    </div>
+  );
 };
 
 export default App;
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,33 @@
-// Write your Character component here
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import CharacterCard from "./CharacterCard";
+
+export default function Character() {
+  const [char, setChar] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://swapi.co/api/people")
+      .then((response) => {
+        console.log(response.data);
+        setChar(response.data.results);
+      })
+      .catch((error) => {
+        console.log("the data was not returned", error);
+      });
+  }, []);
+
+  return (
+    <div>
+      {char.map((data) => {
+        return (
+          <CharacterCard
+            name={data.name}
+            hair={data.hair_color}
+            dob={data.birth_year}
+            home={data.homeworld}
+          />
+        );
+      })}
+    </div>
+  );
+}
